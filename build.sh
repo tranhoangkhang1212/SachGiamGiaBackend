38866a68af0a7b0ae27ab1c64710edec184fecfd
Trần Khang <tranhoangkhang9899@gmail.com>
2023-10-03 06:22:38 +0700

    feat: Always restart container build

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ else
 fi
 
 # Step 2: Run the Docker container
-docker run -d --name ${container_name} -p 8080:3001 sach_giam_gia_be
+docker run -d --name ${container_name} -p 8080:3001 --restart always sach_giam_gia_be
 
 # Kiểm tra xem container có đang chạy không
 if [ $? -eq 0 ]; then
