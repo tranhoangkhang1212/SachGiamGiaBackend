48232e8ee243dc31ddb9c682d636270fde6a037b
Trần Khang <tranhoangkhang9899@gmail.com>
2023-10-03 01:26:47 +0700

    fix: Fix cors

--- src/modules/auth/auth.global.guard.ts
+++ src/modules/auth/auth.global.guard.ts
@@ -18,7 +18,7 @@ export class DefaultAuthGuard implements CanActivate {
 
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      throw new ForbiddenException();
     }
 
     if (!(await this.validateRequest(requestUrl, token))) {
